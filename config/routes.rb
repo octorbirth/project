d56479ebf7ff5e55d05f4cddd9ba01c671654146
Hyeon Seung Woo <hsu9643@naver.com>
2017-08-07 11:41:21 +0000

    view routes seccess

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
 
   get "manager" => "home#manager" #application_controller 수정 후 [로그인 성공] => manager view로
   
+  
+  get 'glist' => "home#glist" # 신청자 목록 페이지
+  get 'tlist' => "home#tlist" # 대여자 목록 페이지
+  get 'item_manager' => "home#itempage" # 아이템 관리 페이지
+  get 'give' => "home#give" # 신청 목록
+  get 'take' => "home#take" # 대여 목록
 end
